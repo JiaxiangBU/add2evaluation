de2e4aab3bd523f7724b5da1052beff7a5a89455
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-08-12 14:33:45 +0800

    Added the function `decile_chart` and document it and make an example in the vignette.

--- R/lift_chart.R
+++ R/lift_chart.R
@@ -15,6 +15,28 @@ lift_chart <- function(table, bin_number = 10){
         )
 }
 
+#' Plot the decile chart for the binary predictions.
+#' @param table The data.frame with two columns named by \code{y} and \code{yhat}.
+#' @param bin_number default 10. The binning set number.
+#' @export
+#' @example
+#' decile_chart(add2evaluation::df, bin_number = 10)
+decile_chart <- function(table, bin_number = 10){
+    global_mean <- mean(table$y)
+    table <- lift_table(table, bin_number = bin_number) %>%
+        mutate(y = y/global_mean)
+    table %>%
+        ggplot() +
+        aes(yhat_bin,y) +
+        geom_col() +
+        theme(
+            axis.text.x = element_text(angle = 90)
+        ) +
+        labs(
+            y = "decile mean / global mean"
+        )
+}
+
 #' Output the dataframe table of lift chart.
 #' @param table The data.frame with two columns named by \code{y} and \code{yhat}.
 #' @param bin_number default 10. The binning set number.
